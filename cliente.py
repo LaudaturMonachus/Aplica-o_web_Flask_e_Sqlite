31196b2c0d8216c338efb11015e0d57cf09fa086
Judson Nunes <123522275+LaudaturMonachus@users.noreply.github.com>
2023-10-17 12:24:54 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,13 @@
+import requests
+
+#data = {"username":"Carlos", "secret":"@dmin456", "cargo": "Engenheiro", "nome":"jose", "salario": "6500"}
+
+#response = requests.post("http://127.0.0.1:5000/register", data=data)
+
+response = requests.get('http://127.0.0.1:5000/empregados')
+
+if response.status_code == 200:
+    message = response.json()
+    print(message['empregados'])
+else:
+    print(response.status_code)
